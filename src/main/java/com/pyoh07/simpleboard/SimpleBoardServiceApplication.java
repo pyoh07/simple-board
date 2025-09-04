daf6892dea51680b9698d73d1b465ae90d2f4838
pyoh07 <pyoh07@gmail.com>
2025-09-04 14:38:38 +0900

    init project

---
+++ src/main/java/com/pyoh07/simpleboard/SimpleBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pyoh07.simpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBoardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pyoh07/simpleboard/SimpleBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyoh07.simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
